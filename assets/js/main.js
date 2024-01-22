470786f885cf0d4d5dd1475e095501a90e25a8ec
Lautaro <laufabio24@gmail.com>
2024-01-22 12:47:42 -0300

    Creamos la Llamada a la API

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,17 @@ const resultContainer = document.querySelector(".resultado_container")
 const parragraphContainer = document.querySelector(".parrafo")
 
 const KEY = "adae4a20143333443419045138aeaa7c";
+
+const requestCity = async city => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&APPID=${KEY}`
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
