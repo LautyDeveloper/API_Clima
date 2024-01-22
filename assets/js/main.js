42cf05bf9615afa3d44eff3a704aaa6940a003b0
Lautaro <laufabio24@gmail.com>
2024-01-22 12:47:04 -0300

    Añadimos objetos de DOM y La KEY para poder usar la API

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#input_ciudad")
+const form = document.querySelector("#form")
+const resultContainer = document.querySelector(".resultado_container")
+const parragraphContainer = document.querySelector(".parrafo")
+
+const KEY = "adae4a20143333443419045138aeaa7c";
